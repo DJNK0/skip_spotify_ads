b7279394f36382fc331c31b82533003def8116e5
DJNK0 <65711556+DJNK0@users.noreply.github.com>
2021-11-02 09:50:03 +0100

    Update spotify.bat

--- spotify.bat
+++ spotify.bat
@@ -1,4 +1,4 @@
 @echo off
 
-START C:\Users\david\AppData\Local\Microsoft\WindowsApps\spotify.exe
-call "F:\Conda\envs\skipads\src\script.bat"
+START path_to_spotify_executable
+call "path_to_script.bat"
