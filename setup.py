3f948f327f0e0433df907c4f33d5e3bb4421211c
DJNK0 <65711556+DJNK0@users.noreply.github.com>
2021-11-03 16:00:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from configparser import ConfigParser, DuplicateSectionError
 import os
 
+#Get working directory
 current_dir = os.getcwd()
 
 def replace_slashes(string):
@@ -12,15 +13,18 @@ def replace_slashes(string):
         index += 1
     return string
 
+#Initialize configparser
 config = ConfigParser()
 config.read('config.ini')
 
+#Create  config.ini file
 try:
     config.add_section('main')
 except DuplicateSectionError:
     print("")
 config.set('main', 'first_time', 'yes')
 
+#Setup messages to the user
 print("Enter the path to your spotify executable")
 spotify_path = replace_slashes(input())
 
@@ -29,12 +33,14 @@ print("There should be a space in between them and in this exact order, and they
 
 main_py_path = replace_slashes(input())
 
+#Write user input to the file
 config.set('main', 'spotify_path', spotify_path)
 config.set('main', 'main_py_path', main_py_path)
 
 with open('config.ini', 'w') as f:
     config.write(f)
 
+#Create bat files for convenience 
 start_script_bat = open(r'start_script.bat', 'w')
 start_script_bat.write(main_py_path + '"\n' + "pause")
 start_script_bat.close()
